1a23f8d53bf39f97e7a55fc2bdd4050226f0b7b9
Admiralfeb <Admiralfeb@users.noreply.github.com>
2020-06-12 03:16:25 +0000

    deploy: 2ac4f9b4eb5b900ea1b743f604c6e88ab080eb3c

---
+++ precache-manifest.69d851db2066da2f118d8fa753ec361b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c1ff33be704f48d04070da3fe257fb7",
+    "url": "/restaurant-finder/index.html"
+  },
+  {
+    "revision": "591719559b7d4d04936b",
+    "url": "/restaurant-finder/static/css/main.a689ad72.chunk.css"
+  },
+  {
+    "revision": "651cf2518054a14a36b4",
+    "url": "/restaurant-finder/static/js/2.771d7189.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/restaurant-finder/static/js/2.771d7189.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "591719559b7d4d04936b",
+    "url": "/restaurant-finder/static/js/main.d8ccefe3.chunk.js"
+  },
+  {
+    "revision": "77e613024aa5b915d25e",
+    "url": "/restaurant-finder/static/js/runtime-main.1231e224.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/restaurant-finder/static/media/logo.5d5d9eef.svg"
+  }
+]);
